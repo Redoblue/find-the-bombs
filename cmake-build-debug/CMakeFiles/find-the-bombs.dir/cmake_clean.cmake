0faa0d421d59cbedccf4bcf38a470465ac26e106
Redoblue <redoblue@hotmail.com>
2017-05-17 21:25:47 +0800

    added build folder

---
+++ cmake-build-debug/CMakeFiles/find-the-bombs.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find-the-bombs.dir/main.cpp.o"
+  "CMakeFiles/find-the-bombs.dir/colorhistogram.cpp.o"
+  "CMakeFiles/find-the-bombs.dir/commander.cpp.o"
+  "CMakeFiles/find-the-bombs.dir/dog.cpp.o"
+  "CMakeFiles/find-the-bombs.dir/police.cpp.o"
+  "find-the-bombs.pdb"
+  "find-the-bombs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find-the-bombs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/find_the_bombs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for find_the_bombs.
+# This may be replaced when dependencies are built.
